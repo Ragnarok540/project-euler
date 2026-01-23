c78016075be5930f9583473fb68a283993e38428
Edgar Nova <ragnarok540@gmail.com>
2026-01-22 19:28:16 -0500

    problem 1 haskell

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # project-euler
 Solutions to Project Euler Challenges
+
+
+- https://projecteuler.net/
+- https://github.com/lucky-bai/projecteuler-solutions
---
+++ haskell/0001.hs
@@ -0,0 +1,21 @@
+-- If we list all the natural numbers below 10 that
+-- are multiples of 3 or 5, we get 3, 5, 6 and 9.
+-- The sum of these multiples is 23.
+-- Find the sum of all the multiples of 3 or 5 below 1000.
+
+divByThreeOrFive :: Integer -> Bool
+divByThreeOrFive n = (mod n 3) == 0 || (mod n 5) == 0
+
+
+sequ :: (Integer -> Bool) -> Integer -> [Integer]
+sequ func start =
+    if func start then start:(sequ func (start + 1)) else sequ func (start + 1)
+
+-- ghci
+-- :load 0001.hs
+-- divByThreeOrFive 3
+-- divByThreeOrFive 4
+-- divByThreeOrFive 5
+-- take 467 (sequ divByThreeOrFive 0)
+-- foldl (+) 0 (take 467 (sequ divByThreeOrFive 0))
+-- 233168
