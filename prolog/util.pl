39ea629308405764157e536b2765d682f9ff9bbd
Edgar Nova <ragnarok540@gmail.com>
2026-03-13 12:38:22 -0500

    problem 96 prolog

--- prolog/util.pl
+++ prolog/util.pl
@@ -52,3 +52,19 @@ count(Goal, List, Count) :-
     length(Included, Count), !.
 
 % count(is_odd, [1,2,3,4,5,6], Count).
+
+equal_lengths([]).
+equal_lengths([[]]).
+equal_lengths([[_|_]]).
+equal_lengths([X,Y|Rest]) :- 
+    length(X, Len), 
+    length(Y, Len), 
+    equal_lengths([Y|Rest]).
+
+transpose([[]|_], []).
+transpose(Matrix, [Row|Rows]) :-
+    transpose_1st_col(Matrix, Row, RestMatrix),
+    transpose(RestMatrix, Rows).
+transpose_1st_col([], [], []).
+transpose_1st_col([[H|T]|Rows], [H|Hs], [T|Ts]) :-
+    transpose_1st_col(Rows, Hs, Ts).
